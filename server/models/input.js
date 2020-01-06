a23f0254c3b93dc922a767af514b291fa4b563aa
Matias Salimbene <msalimbe@ar.ibm.com>
2020-01-06 08:50:03 -0300

    kickoff politicos

--- server/models/input.js
+++ server/models/input.js
@@ -4,13 +4,13 @@ const { Schema } = mongoose;
 Joi.objectId = require("joi-objectid")(Joi);
 
 const inputSchema = new Schema({
-  left: { type: String, required: true, trim: true },
-  right: { type: String, required: true, trim: true },
   author: { type: String, required: true, trim: true },
-  position: { type: String, required: true, trim: true },
-  source: [{ type: String, trim: true }],
-  tags: [{ type: String, trim: true }],
+  texts: [{ type: String, trim: true }],
+  dates: [{ type: Date, default: Date.now }],
+  titles: [{ type: String, required: true, trim: true }],
+  sources: [{ type: String, trim: true }],
   links: [{ type: String, trim: true }],
+  tags: [{ type: String, trim: true }],
   views: { type: Number, required: true, default: 0 },
   submmited: { type: Date, default: Date.now },
   updated: { type: Date, default: Date.now }
@@ -20,11 +20,12 @@ const Input = mongoose.model("Input", inputSchema);
 
 function validateInputSchema(quote) {
   const schema = {
-    left: Joi.string().required(),
-    left: Joi.string().required(),
     author: Joi.string().required(),
+    texts: Joi.array().required(),
+    dates: Joi.array().required(),
+    titles: Joi.array().required(),
     sources: Joi.array().required(),
-    tags: Joi.array().required()
+    links: Joi.array().required()
   };
 
   return Joi.validate(input, schema);
