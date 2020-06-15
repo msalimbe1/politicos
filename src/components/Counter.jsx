44c1d8f62f1e1d44c1ec44416be5bb32ef600d61
Matias Salimbene <msalimbe@ar.ibm.com>
2020-06-15 20:07:37 -0300

    refactored to hooks and context

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import withBlock from "./hoc/withBlock";
+
 import "../styles/counter.scss";
+import logo from "../images/logo.png";
 
 const Counter = ({ days, showTooltip }) => {
   console.log(showTooltip);
-  return <div className="counter__container">{days}</div>;
+  return (
+    <div className="counter__container">
+      {`${days}`}
+      <img src={logo} alt="cuarentini" />
+    </div>
+  );
 };
 
 export default withBlock(Counter);
--- src/components/context/tiniContext.js
+++ src/components/context/tiniContext.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TiniContext = React.createContext({
-  days: (new Date() - new Date("03/20/20")) / (1000 * 60 * 60 * 24),
+  days: Math.floor((new Date() - new Date("03/20/20")) / (1000 * 60 * 60 * 24)),
 });
 
 export default TiniContext;
