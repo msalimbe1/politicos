6dbad12e45153614c02b34f8f184d7c7df650cf1
Matias Salimbene <msalimbe@ar.ibm.com>
2020-06-17 12:48:56 -0300

    floor to ceil

--- src/components/context/tiniContext.js
+++ src/components/context/tiniContext.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TiniContext = React.createContext({
-  days: Math.floor((new Date() - new Date("03/20/20")) / (1000 * 60 * 60 * 24)),
+  days: Math.ceil((new Date() - new Date("03/20/20")) / (1000 * 60 * 60 * 24)),
 });
 
 export default TiniContext;
