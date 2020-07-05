10ff850a951abd2b6a5f95b2e0457f45bae335d7
Matias Salimbene <msalimbe@ar.ibm.com>
2020-07-05 18:20:24 -0300

    mayor changes

--- server/startup/routes.js
+++ server/startup/routes.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const inputs = require("../routes/inputs");
-// const home = require("../routes/home");
+const quotes = require("../routes/quotes");
+const users = require("../routes/users");
 const error = require("../middleware/error");
+const auth = require("../routes/auth");
 
 module.exports = function (app, env) {
   app.use(express.json());
 
-  app.use("/api/inputs", inputs);
+  app.use("/api/quotes", quotes);
+  app.use("/api/users", users);
+  app.use("/api/auth", auth);
 
   app.use(error);
   if (env !== "test") console.log("[routes] api endpoints loaded");
