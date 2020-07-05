4963348a61e7cbe816184575fb88272c4efc661d
Matias Salimbene <msalimbe@ar.ibm.com>
2020-07-05 18:30:33 -0300

    protected backend

--- server/routes/quotes.js
+++ server/routes/quotes.js
@@ -1,21 +1,21 @@
 const { Quote, validate } = require("../models/quote");
-
+const auth = require("../middleware/auth");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req, res) => {
   const quotes = await Quote.find();
   console.log("Getting all quotes...");
   res.send(quotes);
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", auth, async (req, res) => {
   const { id } = req.params;
   const quote = await Quote.findById(id);
   res.send(quote);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", auth, async (req, res) => {
   console.log(req.body);
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error);
